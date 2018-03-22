2f1d4dc9e2eb713b225c604f61a9a29b5ac7d41a
Adrian Morente <adrian95morente@gmail.com>
2018-03-22 19:59:01 +0100

    no funciona, solo añado comentarios intentando planificar la estructura

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PracticasSG
+
+La práctica 2 consistirá en realizar un juego cuyo protagonista será una copia barata de R2D2, el personaje de Star Wars.
+
+Para ejecutar el proyecto, puede ejecutarse (Linux) en el directorio raíz del proyecto:
+
+`$ python3 -m http.server 8000`
--- r2d2/R2D2.js
+++ r2d2/R2D2.js
@@ -13,17 +13,22 @@ class R2D2 extends THREE.Object3D {
   constructor (parameters) {
     super();
 
-    //Parametros para las dimensiones del robot
+    // Parámetros para las dimensiones del robot
     this.height = (parameters.height === undefined ? 25 : parameters.height);
     this.width = this.height/2;
     this.material = (parameters.material === undefined ?
-        new THREE.MeshPhongMaterial ({color: 0xd4af37, specular: 0xfbf804, shininess: 70}) : this.material);
+        new THREE.MeshPhongMaterial ({color: 0xffffff, specular: 0xccccff, shininess: 70}) : this.material);
 
-    //Cabeza, tronco y brazos
+    // Partes del cuerpo
+    this.len = null;
     this.head = null;
     this.trunk = null;
-    this.rightArm = null;
-    this.leftArm = null;
+    this.rightShoulder = null;
+    this.rightForearm = null;
+    this.rightFoot = null;
+    this.leftShoulder = null;
+    this.leftForearm = null;
+    this.leftFoot = null;
 
     //Dimensiones de los pies
     this.footHeight = this.height*0.1;
@@ -38,14 +43,62 @@ class R2D2 extends THREE.Object3D {
     this.shoulderDims = this.width*0.15;
 
     this.trunk = this.createBody();
-    this.add(this.head);
-    this.add(this.trunk);
+  }
+
+  createBody(){
+    /*
+    * crear lente
+    * trasladar lente
+    * añadir lente a cabeza
+    * rotar cabeza
+    * trasladar cabeza
+    * añadir cabeza a tronco
+    * rotar tronco
+    * trasladar tronco
+    * añadir tronco a cuerpo
+    * */
+
+    /*
+    * crear hombro
+    * trasladar hombro
+    * crear brazo
+    * añadir hombro a brazo
+    * trasladar hombro
+    * crear pie
+    * añadir hombro a pie
+    * trasladar pie a x y -x
+    * añadir pies a cuerpo
+    * */
+
+    //lente (cilindro)
+    var len = new THREE.Mesh(new THREE.CylinderGeometry(this.armRadius, this.armRadius, 1, 16,32),this.material);
+    len.geometry.applyMatrix (new THREE.Matrix4().makeRotationY (Math.PI / 90));
+    len.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (this.width/2, this.height,10));
+
+    //cabeza (semiesfera)
+    this.head = new THREE.Mesh(new THREE.SphereGeometry(this.width/2.5,32,32),this.material);
+    this.head.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (this.width/1, this.height/1,0));
+    this.head.add(len);
+
+    //Creacion de los dos brazos
+    this.rightFoot = this.createShoulder();
+    this.leftFoot = this.createShoulder();
+    this.leftFoot.position.x = this.width;
+
+    this.add(this.rightFoot);
+    this.add(this.leftFoot);
+
+    //tronco (cilindro)
+    this.trunk = new THREE.Mesh(
+      new THREE.CylinderGeometry(this.width/2.25,this.width/2.25, this.height/2,32,32),this.material);
+    this.trunk.add(this.head);
+    this.trunk.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (this.width/2, this.height/2,0));
+    return this.trunk;
   }
 
 
   //Metodo que crea el hombro del robot
   createShoulder(){
-
     var shoulder = new THREE.Mesh (
         new THREE.BoxGeometry (this.shoulderDims, this.shoulderDims, this.shoulderDims),
         this.material);
@@ -58,10 +111,8 @@ class R2D2 extends THREE.Object3D {
 
   //Metodo que crea el brazo del robot
   createArm(){
-
     var arm = new THREE.Mesh(
         new THREE.CylinderGeometry(this.armRadius,this.armRadius,this.armHeight,16,32),this.material);
-
     arm.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (0,3*this.footHeight, 0));
     arm.castShadow=true;
     arm.autoUpdateMatrix = false;
@@ -71,7 +122,6 @@ class R2D2 extends THREE.Object3D {
 
   //Metodo para crear el pie del robot, un tronco de cono
   createFoot(){
-
     var foot = new THREE.Mesh(
         new THREE.CylinderGeometry(this.footTopRadius, this.footBottomRadius,this.footHeight,16,32 ), this.material);
     foot.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (0, this.footHeight/2, 0));
@@ -80,46 +130,9 @@ class R2D2 extends THREE.Object3D {
     return foot;
   }
 
-
-  createBody(){
-    //lente (cilindro)
-    var len = new THREE.Mesh(new THREE.CylinderGeometry(this.armRadius, this.armRadius, 1, 16,32),this.material);
-    len.geometry.applyMatrix (new THREE.Matrix4().makeRotationY (Math.PI / 180));
-    len.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (this.width/2, this.height,10));
-
-    //cabeza (semiesfera)
-    this.head = new THREE.Mesh(new THREE.SphereGeometry(this.width/2.5,32,32),this.material);
-    this.head.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (this.width/2, this.height/1.3,0));
-    this.head.add(len);
-
-    //Creacion de los dos brazos
-    this.rightArm = this.createShoulder();
-    this.leftArm = this.createShoulder();
-    this.leftArm.position.x = this.width;
-
-    this.add(this.rightArm);
-    this.add(this.leftArm);
-
-    //tronco (cilindro)
-    this.trunk = new THREE.Mesh(
-        new THREE.CylinderGeometry(this.width/2.25,this.width/2.25, this.height/2,32,32),this.material);
-    this.trunk.add(this.head);
-    this.trunk.geometry.applyMatrix (new THREE.Matrix4().makeTranslation (this.width/2, this.height/2,0));
-    return this.trunk;
-  }
-
 }
 
 /*
-  crear R2D2:
-    - crear brazos completos:
-      - crear pies completos:
-      - crear hombro+brazo:
-        - crear hombro.
-        - crear brazo.
-    - crear tronco+cabeza completos:
-
-
 1) cabeza = new Mesh(esf, m)
 2) cuerpo = new Mesh(cilin, m)
 3) cuerpo.rotation.x = __
